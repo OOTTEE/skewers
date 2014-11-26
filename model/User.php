989ec8a4e805fdedbf6ea52ff54d28e1d9e00e6c
Hector <hectornovou@gmail.com>
2014-11-26 17:29:46 +0100

    cambios user.php

--- model/User.php
+++ model/User.php
@@ -1,4 +1,5 @@
 <?php 
+include_once($GLOBALS['MODEL_PATH'].'Model.php';
 class User extends Model{
 	public $usuario_id;
 	public $name;
@@ -44,6 +45,22 @@ class User extends Model{
 	
 	
 	public function getUser($id){
+		
+		
+		$sentencia= $GLOBALS['DB']->prepare(SELECT usuario_id, name, username, role, phone
+								FROM users
+								WHERE username = ? );
+		$sentencia=$GLOBALS['DB']->execute(array($id));
+		$resul=$sentencia->fetchall()[0];
+		$object= new User();
+		
+		$object->usuario_id=$resul['usuario_id'];
+		$object->name=$resul['name'];
+		$object->user=$resul['username'];
+		$object->role=$resul['role'];
+		$object->phone=$resul['phone'];
+		
+		return $object;
 			
 	}
 
