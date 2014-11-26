818ed0d60ef4a7cf3b2ec5efaf3e8fc36a051cce
Hector <hectornovou@gmail.com>
2014-11-26 17:40:10 +0100

    cambio index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 ﻿<?php 
 include_once($_SERVER['DOCUMENT_ROOT'].'/lib/php/includes.php');
 
-include_once($LAYOUT_PATH.'header.php');
+include_once($GLOBALS['LAYOUT_PATH'].'header.php');
 
-include_once($LAYOUT_PATH.'notLoginNav.php');
+include_once($GLOBALS['LAYOUT_PATH'].'notLoginNav.php');
 	
 if(isset($_REQUEST['action']) AND $_REQUEST['action'] == 'register'){
-	include_once($TEMPLATES_PATH.'index/register.php');
+	include_once($GLOBALS['TEMPLATES_PATH'].'index/register.php');
 }else{
-	include_once($TEMPLATES_PATH.'index/index.php');
+	include_once($GLOBALS['TEMPLATES_PATH'].'index/index.php');
 }
 	
-include_once($LAYOUT_PATH.'footer.php');
+include_once($GLOBALS['LAYOUT_PATH'].'footer.php');
 	
 session_write_close();
 ?>
