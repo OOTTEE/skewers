f5b2478d27e5d56c6a9edb0137ba73f13fbf5cb9
Anibal <anibalmgb1992@gmail.com>
2014-11-27 11:47:21 +0100

    He creado el index y la carpeta de Jurado Popular.

---
+++ template/popular/index.php
@@ -0,0 +1,22 @@
+
+ <div class="jumbotron">
+      <div class="container">
+        <h1>Jurado Popular</h1>
+       
+        
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          
+          <a class="btn btn-default" href="#" role="button">Votar  pincho</a></p>
+		  <a class="btn btn-default" href="#" role="button">Marcar  pincho</a></p>
+		  <a class="btn btn-default" href="#" role="button">Comentar pincho</a></p>
+		  <a class="btn btn-default" href="#" role="button">Consultar pincho</a></p>
+        </div>
+		</div>
+    </div>
+        
