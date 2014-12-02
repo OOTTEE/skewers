77f0a3b280ecef78eda1f09074c432717bec89e9
Javier Lorenzo Martin <jlmartin@esei.uvigo.es>
2014-12-02 19:56:55 +0100

    Apañada vista validar 1 pinchos

--- template/admin/realizarValidacionPincho.php
+++ template/admin/realizarValidacionPincho.php
@@ -6,27 +6,47 @@
     </div>
 
     <div class="container">
-	<form class="form-signin" role="form"  Method="POST" action="<?php echo $GLOBALS['CONTROLLER_URL'].'adminController.php';?>" >      
-	<?php 	echo "Pinchos_id : ".$pincho->pincho_id."<br>";
-		echo "Usuario_id : ".$pincho->usuario_id."<br>";
-		echo "Ingredientes : ".$pincho->ingredientes."<br>";
-		echo "Nombre : ".$pincho->nombre."<br>";
-		echo "Precio : ".$pincho->precio."€";echo "<br>";
-		echo "Finalista : "; if($pincho->finalista==0)
-					echo "No";
-				     else 
-					echo "Si";
-
-; echo "<br>";
-		if($pincho->imagen<>null){
-			echo "Imagen : ".$pincho->imagen."<br>";
-		}
-		echo "Descripcion : ".$pincho->descripcion."<br>";
-		?>
-<input type="hidden" name="pincho_id" value="<?php echo $pincho->pincho_id ?>">
-<button class="btn btn-lg btn-primary btn-block" type="submit"  value="validar" name="action" >VALIDAR PINCHO</button>
-<button class="btn btn-lg btn-primary btn-block" type="submit"  value="volver" name="action" >VOLVER</button>
+	<form class="form-signin" role="form"  Method="POST" action="<?php echo $GLOBALS['CONTROLLER_URL'].'adminController.php';?>" >
+		<div class="panel panel-default">
+		  <div class="panel-heading"><?= $pincho->nombre ?></div>
+		  <div class="panel-body">
+				<table class="table">
+					<tr>
+						<td>Pinchos_id:</td>
+						<td><?= $pincho->pincho_id ?></td>
+					</tr>
+					<tr>
+						<td>Usuario_id:</td>
+						<td><?= $pincho->usuario_id ?></td>
+					</tr>
+					<tr>
+						<td>Ingredientes:</td>
+						<td><?= $pincho->ingredientes ?></td>
+					</tr>
+					<tr>
+						<td>Precio:</td>
+						<td><?= $pincho->precio ?>€</td>
+					</tr>
+					<tr>
+						<td>Finalista:</td>
+						<td><?= ($pincho->finalista==0)? 'No' : 'Si' ?></td>
+					</tr>
+					<tr>
+						<td>Imagen:</td>
+						<td><?= ($pincho->imagen<>null)? $pincho->imagen : '' ?></td>
+					</tr>
+					<tr>
+						<td>Descripcion:</td>
+						<td><?= $pincho->descripcion ?></td>
+					</tr>
+				</table>
 		
+				<input type="hidden" name="pincho_id" value="<?php echo $pincho->pincho_id ?>">
+				<button class="btn btn-lg btn-primary btn-block" type="submit"  value="validar" name="action" value="" >Validar Pincho <span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></button>
+				<button class="btn btn-lg btn-primary btn-block" type="submit"  value="volver">Volver</button>
+		
+		  </div>
+		</div>
 	</form>
 <?php
 
